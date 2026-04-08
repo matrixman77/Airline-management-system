a462f800ddb6ee0688d1185603575da0e21a3b14
jjm2092-hub <jjm2092@gmail.com>
2026-04-07 18:19:20 -0600

    Update ScheduleInformation.java

--- Stage 3/ScheduleInformation.java
+++ Stage 3/ScheduleInformation.java
@@ -1,3 +1,7 @@
+/**
+ * @author Jonathon Martinez
+ * This class is used to store information about a flight schedule.
+ */
 public class ScheduleInformation {
     private short departureTime;
     private short arrivalTime;
@@ -5,23 +9,45 @@ public class ScheduleInformation {
     private short cancelledFlight;
     private short boardingTime;
 
+    /**
+     * Constructor for ScheduleInformation class.
+     * @param departureTime The departure time of the flight.
+     * @return The departure time.
+     */
     public short getDepartureTime() {
         return departureTime;
     }
 
+    /**
+     * Gets the arrival time of the flight.
+     * @return The arrival time.
+     */
     public short getArrivalTime() {
         return arrivalTime;
     }
 
+    /**
+     * Gets the delayed flight status.
+     * @return The delayed flight status.
+     */
     public short getDelayedFlight() {
         return delayedFlight;
     }
 
+    /**
+     * Gets the cancelled flight status.
+     * @return The cancelled flight status.
+     */
     public short getCancelledFlight() {
         return cancelledFlight;
     }
 
+    /**
+     * Gets the boarding time of the flight.
+     * @return The boarding time.
+     */
     public short getBoardingTime() {
         return boardingTime;
     }
 }
+
