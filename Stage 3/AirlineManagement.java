58ef2067a142ff826dd329d65196ca30e718ac18
jjm2092-hub <jjm2092@gmail.com>
2026-04-07 18:18:40 -0600

    Update AirlineManagement.java

--- Stage 3/AirlineManagement.java
+++ Stage 3/AirlineManagement.java
@@ -1,42 +1,71 @@
+/**
+ * @author Jonathon Martinez
+ * This class is used to manage the airline information.
+ */
 public class AirlineManagement {
-    //private ArrayList<planeType> planes;
     private ArrayList<flightNumber> flightNumbers;
     private ArrayList<gateNumber> gateNumber;
     private ArrayList<airName> airName;
 
+    /**
+     * Constructor for AirlineManagement class.
+     */
     public AirlineManagement() {
         flightNumbers = new ArrayList<>();
         gateNumber = new ArrayList<>();
         airName = new ArrayList<>();
     }
 
+    /**
+     * Adds a flight to the airline management system.
+     * @param flightNumber
+     * @param gateNumber
+     * @param airName
+     */
     public void addFlight(String flightNumber, String gateNumber, String airName) {
         this.flightNumbers.add(flightNumber);
         this.gateNumber.add(gateNumber);
         this.airName.add(airName);
     }
 
-    //public addPlaneType() {}
-
+    /**
+     * Adds a terminal number to the airline management system.
+     * @param gateNumber The terminal number to add.
+     */
     public void addTerminalNumber(String gateNumber) {
         this.gateNumber.add(gateNumber);       
     }
 
+    /**
+     * Adds an airline name to the airline management system.
+     * @param airName The airline name to add.
+     */
     public void addAirlineName(String airName) {
         this.airName.add(airName);
     }
 
+    /**
+     * Removes a flight from the airline management system.
+     * @param flightNumber
+     */
     public void removeFlightNumber(String flightNumber) {
         this.flightNumbers.remove(flightNumber);
     }
 
-    //public void removePlaneTYpe(String planeType) {}
-
+    /**
+     * Removes a terminal number from the airline management system.
+     * @param gateNumber
+     */
     public void removeTerminalNumber(String gateNumber) {
         this.gateNumber.remove(gateNumber);
     }
 
-    public void AirlineName(String airName) {
+    /**
+     * Removes an airline name from the airline management system.
+     * @param airName The airline name to remove.
+     */
+    public void removeAirlineName(String airName) {
         this.airName.remove(airName);
     }
 }
+
