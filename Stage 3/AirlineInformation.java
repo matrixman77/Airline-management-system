fa07934356245f91776b75b80cbc062252ade44f
jjm2092-hub <jjm2092@gmail.com>
2026-04-07 18:17:41 -0600

    Update AirlineInformation.java

--- Stage 3/AirlineInformation.java
+++ Stage 3/AirlineInformation.java
@@ -1,21 +1,42 @@
+/**
+ * @author Jonathon Martinez
+ * This class is used to store information about an airline.
+ * 
+ */
 public class AirlineInformation {
     private String flightNumber;
     private String planeType;
     private String terminalNumber;
     private String airlineName;
     
+    /**
+     * Constructor for AirlineInformation class.
+     * @param flightNumber The flight number of the airline.
+     */
     public getFlightNumber() {
         return flightNumber;
     }
 
+    /**
+     * Gets the plane type of the airline.
+     * @return The plane type.
+     */
     public getPlaneType() {
         return planeType;
     }
 
+    /**
+     * Gets the terminal number of the airline.
+     * @return The terminal number.
+     */
     public getTerminalNumber() {
         return terminalNumber;
     }
 
+    /**
+     * Gets the airline name.
+     * @return The airline name.
+     */
     public getAirlineName() {
         return airlineName;
     }
