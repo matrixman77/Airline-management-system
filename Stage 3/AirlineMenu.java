9e11a5a38c668f6e227da02b5fe34c2717b567d0
jjm2092-hub <jjm2092@gmail.com>
2026-04-11 17:19:14 -0600

    Update AirlineMenu.java

--- Stage 3/AirlineMenu.java
+++ Stage 3/AirlineMenu.java
@@ -506,10 +506,35 @@ public static void adminMenu()
 		System.out.println("Boarding time updated.");
     }
     
-    public static void displaySchedule()
-    {
-    
-    }
+    public static void viewDepartureTimes() {
+		for (short time : departureTimes) {
+			System.out.println(time +" ")
+		}
+	}
+	
+	public static void viewArrivalTimes() {
+		for (short time : ArrivalTimes) {
+			System.out.println(time +" ")
+		}
+	}
+	
+	public static void viewDelayedFlights() {
+		for (short flight : delayedFlights) {
+			System.out.println(flight +" ")
+		}
+	}
+	
+	public static void viewCancelledFlights() {
+		for (short flight : cancelledFlights) {
+			System.out.println(flight +" ")
+		}
+	}
+	
+	public static void viewBoardingTimes() {
+		for (short time : boardingTimes) {
+			System.out.println(time +" ")
+		}
+	}
 	
 	public static boolean checkPassword()
    {
